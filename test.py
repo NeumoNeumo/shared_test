44cced40cc99a80faa5eeacf8fdeb3e834f251a3
Jianhui Yuan Xu <neuromoments@gmail.com>
2023-02-18 11:38:11 +0800

    let me have lunch first :)

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import json
 import argparse
+import subprocess
 
 epilog = """With no options, test will read from the config file (./test.conf
 by default) automatically test your executable according to the testcase in
@@ -8,21 +10,20 @@ two-digit numbers and <problemID> is a lowercase letter.
 
 To change the default setting, the following options can be used.
 
-   *`-r` tests your program with random cases if `random.py` or `random.out` is
-    provided in `<labID>/test/<problemID>/<testcaseID>/`. When both of these two
-    files exist, `random.out` is prefered in consideration of efficiency.
-    `random.*` must inherit python class `TestSet` in this file or
-    `TestSet.cpp`
+   *`-r` tests your program with random cases if `random_test.py` or
+    `random_test.out` is provided in `<labID>/test/<problemID>/<testcaseID>/`.
+    When both of these two files exist, `random_test.out` is prefered in
+    consideration of efficiency. If using python module, there must be a class
+    `RandomTestSet` in module `random_test.py` inheriting from `TestSet` in this
+    file.
 
    *`-R` behaves the same as `-r` except for using default testcases as well.
 
-   *`-m` helps to match your result with that of other students. Either full
-    studentID or the last few digits, if no ambiguity (usually last 4 is
-    enough), is acceptable.
+   *`-m SID` helps to match your result with that of other students with SID as
+    his or her studentID. Either full studentID or the last few digits, if no
+    ambiguity (usually last 4 is enough), is acceptable.
 
    *`-l` helps to list all the available executables you can match with.
-
-   *``
 
 Configuration file describes which problem you are dealing with. It is in the
 format of json, containing labID and problemID.
@@ -32,7 +33,8 @@ format of json, containing labID and problemID.
 class TestSet:
     """This is the class which all customized test sets should inherit"""
 
-    def __init__(self, labID, problemID):
+    def __init__(self, name, labID, problemID):
+        self.name = name
         self.labID = labID
         self.problemID = problemID
 
@@ -40,6 +42,14 @@ class TestSet:
         return self
 
     def __next__(self):
+        """
+        Returns: (unit_case, verifier)
+            unit_case (string): directed to stdin of the tested program
+            verifier (string|Callable[[string], bool]) : either a string used to
+                match the stdout or a function to verify whether the output is
+                reasonable (usually used when the problem has multiple
+                solutions)
+        """
         raise NotImplementedError()
 
 
@@ -47,10 +57,10 @@ class DefaultTestSet(TestSet):
     """get test data from given fils in <labID>/test/<problemID>/<testcaseID>"""
 
     def __init__(self, labID, problemID):
-        self.labID = labID
-        self.problemID = problemID
-        self.data_dir = Path().cwd()/labID/"test"/problemID
-        
+        super().__init__("Default", labID, problemID)
+        self.data_dir = Path().cwd() / labID / "test" / problemID
+
+
 class Info:
     _RST = "\033[0m"
     _RED = "\033[1;31m"
@@ -64,25 +74,62 @@ class Info:
     def error(txt):
         print(Info._RED + "Error: " + Info._RST + txt)
 
-
 def main(args):
-    conf = Path("test.conf")
-    if not conf.is_file():
+    conf_p = Path("test.conf")
+    if not conf_p.is_file():
         print("lala")
         Info.warn("Config file not found.")
         yn = input("Confirm to init config? (Y/n)")
         if yn == "n" or yn == "N" or yn == "NO" or yn == "No":
-            print("no")
-        elif yn == "y" or yn == "Y" or yn == "YES" or yn == "Yes":
-            print("yes")
+            return
+        elif yn == "y" or yn == "Y" or yn == "YES" or yn == "Yes" or yn == "":
+            labID = input("Please input your labID (lab**): ")
+            problemID = input("Please input your problemID (lowercase single letter): ")
+            with open(conf_p, "w") as f:
+                f.write('{"labID":%s,"problemID":%s}'.format(labID, problemID))
         else:
             Info.error("Invalid option")
             return
+    with open(conf_p, "r") as f:
+        conf = json.load(f)
+    labID = conf["labID"]
+    problemID = conf["problemID"]
+    testset_opt = list()
+    if not args.random and not args.Random:
+        testset_opt = ["default"]
+    elif args.Random:
+        testset_opt = ["default", "random"]
+    elif args.random:
+        testset_opt = ["random"]
 
+    test_basedir = Path.cwd()/labID/"test"/problemID
+    if not test_basedir.is_dir():
+        Info.error(f"{str(test_basedir)} not a directory. It is possible that nobody has written a testcase yet ")
+        return
+    testsets = list()
+    if "default" in testset_opt:
+        testsets.append(DefaultTestSet(labID, problemID))
+    if "random" in testset_opt:
+        import sys
+        sys.path.insert(1, str(test_basedir))
+        try:
+            from random_test import RandomTestSet # type: ignore
+        except Exception:
+            Info.error(f"random_test.* not found under {str(test_basedir)}")
+            return
+        testsets.append(RandomTestSet(labID, problemID))
+    if not args.match:
+        for testset in testsets:
+            print(f"Testing in testset {testset.name}")
+            for unit_case, verifier in testset:
+                subprocess.run()
+    else:
+        pass
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        usage="test [-h] [-rRml] EXEC",
+        usage="test [-h] [-rRl] [-m SID] EXEC",
         prog="test",
         description="A simple tool for test cpp codes.",
         epilog=epilog,
@@ -92,7 +139,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-R", "--Random", action="store_true", help="random test and default test"
     )
-    parser.add_argument("-m", "--match", action="store_true", help="matching test")
+    parser.add_argument("-m", "--match", metavar="SID", help="matching test with another")
     parser.add_argument(
         "-l", "--list-available", action="store_true", help="list available executables"
     )
